8dcb9b6c89b3edda81de4922f909873233ed69e1
peauli <preciousajorgba@gmail.com>
2022-10-30 14:03:01 +0100

    deleted fasta.py

--- task1.py
+++ task1.py
@@ -5,7 +5,6 @@ app = FastAPI()
 
 
 
-#/app.get
 @app.get("/")
 async def home():
     info = { "slackUsername": "peauli", "backend": True, "age": 28, "bio": "I am a backend developer in the HNG9 internship" }
