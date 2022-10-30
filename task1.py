feb17d057ab3f471b0af05abee8b07f2df0b5ac2
peauli <preciousajorgba@gmail.com>
2022-10-30 15:41:35 +0100

    changed true to True in info

--- task1.py
+++ task1.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def home():
-    info = { "slackUsername": "peauli", "backend": True, "age": 28, "bio": "I am a backend developer in the HNG9 internship" }
+    info = { "slackUsername": "peauli", "backend":  True, "age": 28, "bio": "I am a python backend developer in the HNG9 internship" }
     return info
 
 
